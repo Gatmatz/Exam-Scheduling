d005498940e3165b437ec3e55eeb6560cf92c9b3
Georgios Atmatzidis <gatmatzidi@gmail.com>
2023-05-10 22:38:38 +0300

    Created courses exam predicate

--- code.pl
+++ code.pl
@@ -1,1 +1,15 @@
+%Open data file with facts.
 ?-consult('attends.pl').
+
+%Predicate that removes duplcate elements from a list.
+remove_duplicates([],[]).
+remove_duplicates([H|T],Result) :-
+	member(H,T),
+	remove_duplicates(T,Result), !.
+remove_duplicates([H|T],[H|Result]) :-
+	remove_duplicates(T,Result).
+
+%Predicate that creates a list "Result" with all the separate exam courses.
+courses(Result) :-
+	findall(X, attends(_,X),L),
+	remove_duplicates(L,Result).
