ad4c96c645ec212195c831ed38cffed0bb3743b1
dlitsidis <dlitsidis@csd.auth.gr>
2023-05-11 22:02:05 +0300

    k-permutations

--- code.pl
+++ code.pl
@@ -13,3 +13,11 @@ remove_duplicates([H|T],[H|Result]) :-
 courses(Result) :-
 	findall(X, attends(_,X),L),
 	remove_duplicates(L,Result).
+
+%Predicate that returns all possible orders of k elements.
+k_permutation(0,_,[]).
+k_permutation(K,L1,[X|T2]) :-
+    K > 0,
+    K1 is K - 1,
+    delete(X,L1,L2),
+    k_permutation(K1,L2,T2).
